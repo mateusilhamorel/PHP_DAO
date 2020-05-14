c71b607545586e756d4f1b53f6530879d9c611f7
Mateus <mateusilhamorel@gmail.com>
2020-05-14 11:01:17 -0300

    Mais recursos da classe Usuario.php

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
+//Traz apenas um usuario
+//usuario = new Usuario();
+//usuario->loadById(2);
+//echo $usuario;
+
+//Carrega lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login.
+//$search = Usuario::search("ka");
+//echo json_encode($search);
+
+//Carrega usuario usando login e senha
 $usuario = new Usuario();
-
-$usuario->loadById(2);
-
+$usuario->login("jose", "dendritos");
 echo $usuario;
 
 ?>
